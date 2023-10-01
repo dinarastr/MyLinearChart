8b0f29668b3d35085c5222baebff66e8b9e3189b
dinarastepina <setare497@gmail.com>
2023-10-01 08:01:05 -0400

    added fragment to the container view

--- app/src/main/java/ru/dinarastepina/myapplication/data/ChartRepository.kt
+++ app/src/main/java/ru/dinarastepina/myapplication/data/ChartRepository.kt
@@ -1,10 +1,13 @@
 package ru.dinarastepina.myapplication.data
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import ru.dinarastepina.myapplication.domain.IChartRepository
 
 class ChartRepository: IChartRepository {
     override suspend fun getChartData(): Flow<Int> {
-
+        return flow {
+            emit(0)
+        }
     }
 }
--- app/src/main/java/ru/dinarastepina/myapplication/presentation/linearChart/LinearChartFragment.kt
+++ app/src/main/java/ru/dinarastepina/myapplication/presentation/linearChart/LinearChartFragment.kt
@@ -6,13 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ru.dinarastepina.myapplication.R
+import ru.dinarastepina.myapplication.databinding.FragmentLinearChartBinding
 
 class LinearChartFragment : Fragment() {
+
+    private var _vb: FragmentLinearChartBinding? = null
+    private val vb: FragmentLinearChartBinding
+        get() = _vb!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_linear_chart, container, false)
+    ): View {
+        _vb = FragmentLinearChartBinding.inflate(inflater, container, false)
+
+        return vb.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -21,5 +29,6 @@ class LinearChartFragment : Fragment() {
 
     override fun onDestroy() {
         super.onDestroy()
+        _vb = null
     }
 }
